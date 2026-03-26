d1a67520cef9eff1dbb46be580ceb910c0955778
FedericoCapitanio <107490782+FedericoCapitanio@users.noreply.github.com>
2026-03-26 12:39:03 +0100

    TASK 1 completed

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -20,11 +20,22 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    case class ComplexNumber(re: Double, im: Double)
+    type Complex = ComplexNumber
+
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
+
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case ComplexNumber(re, im) => re
+      def im(): Double = complex match
+        case ComplexNumber(re, im) => im
+      def sum(other: Complex): Complex = (complex, other) match
+        case (ComplexNumber(re, im), ComplexNumber(re2, im2)) => ComplexNumber(re + re2, im + im2)
+      def subtract(other: Complex): Complex = (complex, other) match
+        case (ComplexNumber(re, im), ComplexNumber(re2, im2)) => ComplexNumber(re - re2, im - im2)
+      def asString(): String = complex match
+        case ComplexNumber(0, 0) => "" + 0.0
+        case ComplexNumber(re, 0) => "" + re
+        case ComplexNumber(0, im) => "" + im + "i"
+        case ComplexNumber(re, im) => "" + re + (if im > 0 then " + " else "") + (if im > 0 then im else " - " + (-im)) + "i"
